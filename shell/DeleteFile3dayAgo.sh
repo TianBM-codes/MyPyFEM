5a89f447f00a0efce2a43a59581aa47e493f15d7
tianbaomin <1259438149@qq.com>
2025-09-01 09:53:33 +0800

    Create DeleteFile3dayAgo.sh

---
+++ shell/DeleteFile3dayAgo.sh
@@ -0,0 +1,3 @@
+find /mnt/proj/NBQZJ/tbm -type f -name "theta*_1001.dat" -ctime +6 -delete
+find /mnt/proj/NBQZJ/tbm -type f -name "theta*_1002.dat" -ctime +6 -delete
+find /mnt/proj/NBQZJ/tbm -type f -name "theta*_1003.dat" -ctime +6 -delete
