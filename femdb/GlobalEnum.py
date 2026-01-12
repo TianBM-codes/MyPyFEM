58433a9ec02282dedf6789a951a9ab43938afa41
tianbaomin <39731032+TianBM-codes@users.noreply.github.com>
2026-01-12 08:50:00 +0800

    summary

--- femdb/GlobalEnum.py
+++ femdb/GlobalEnum.py
@@ -17,6 +17,7 @@ mlogger.addHandler(handler)
 # 配置matplot, 关闭其logging
 logging.getLogger('matplotlib.font_manager').disabled = True
 logging.getLogger('matplotlib.pyplot').disabled = True
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
@@ -176,6 +177,7 @@ class AnalyseType(Enum):
     TestFunction = 12
     SteadyThermal = 13
     HeatStress = 14
+    ExpansionOpt = 15
 
 
 class InputFileType(Enum):
